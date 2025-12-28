3e40dfbed8253d28a845d19fc156b57e0be95872
offline2099 <offline2099@protonmail.com>
2025-12-28 13:02:27 +0700

    add top level components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu/menu.component';
+import { EditorComponent } from './components/editor/editor/editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MenuComponent
+  },
+  {
+    path: 'editor',
+    component: EditorComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
