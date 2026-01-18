99d170de70bc56e4dcff5a53801d5d825a233a35
offline2099 <offline2099@protonmail.com>
2026-01-18 16:19:55 +0700

    install electron

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    //frame: false,
+    backgroundColor: '#000',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+
+  win.loadFile(path.join(__dirname, 'dist/snake-game/browser/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.commandLine.appendSwitch('disable-web-security');
