5651bded87469266f202726a15fc13a3e46e44e0
offline2099 <offline2099@protonmail.com>
2026-03-02 17:09:55 +0700

    add button for switching between game and level editor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
+  isEditorOpen$: Observable<boolean>;
+
+  constructor(private router: Router) {
+    this.isEditorOpen$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event.url.includes('/editor'))
+    );     
+  }
   
 }
